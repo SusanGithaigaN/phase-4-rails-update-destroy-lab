d0a72fca810a536e10f2bdb35d3df0d02176e98c
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-15 06:42:38 +0300

    Completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,44 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
-  private
 
+  # PATCH /plants/:id
+  def update
+    # find
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      # update
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  def increment_is_in_stock
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(is_in_stock: plant.is_in_stock + 1)
+      render json: plant
+      else
+        render json: { error: "Plant not found" }, status: :not_found
+      end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+      else
+        render json: { error: "Plant not found" }, status: :not_found
+      end
+  end
+
+  private
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
